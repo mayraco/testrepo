fb16badab0e0ba0b4b9f17a53ca2f05b58e09a78
mayraco <103273127+mayraco@users.noreply.github.com>
2022-04-08 11:02:39 -0500

    Add files via upload

---
+++ quiz4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 17:57:12 2021
+
+@author: mayraacosta
+"""
+
+# for num in range(2,9,2):
+#     print(num)
+
+# special = '1357 cuntry ln.'
+# s_string = special[-3:]
+# print(s_string)
+
+# mystr = 'yes'
+# yourstr = 'no'
+# mystr += yourstr * 2
+# print(mystr)
+
+total = 0
+for count in range(1,32,1):
+    total += count
+    print (total)
